f75833d6ffa985cb80f5990d9c16f6cfec77cfad
Joao Sant Ana <jmpsantana15@gmail.com>
2025-06-04 18:52:26 -0300

    Fix: lint error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { type MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    let url = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+    const url = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 
     const defaultPages: MetadataRoute.Sitemap = [
         {
