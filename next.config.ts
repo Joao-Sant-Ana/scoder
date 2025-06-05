463e87382f844d4db2e8d1dcd5ddaba0f7ad3d7f
Joao Sant Ana <jmpsantana15@gmail.com>
2025-06-05 00:59:40 -0300

    Feat: Added a very simple cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,HEAD,OPTIONS"},
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type, Authorization" },
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
